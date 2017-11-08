64d81514292603967e1fb4220b64a934e7e47951
Marvelousmishig <dmishig@gmail.com>
2017-11-08 14:21:26 -0700

    feat: calc dot vals

--- 10-Sphere Shading/SimpleCanvas.java
+++ 10-Sphere Shading/SimpleCanvas.java
@@ -29,31 +29,50 @@ public class SimpleCanvas extends JPanel{
     // transformation on the entire set
 		g2d.translate(getWidth()/2,getHeight()/2);
     calcPoints();
+    System.out.println("done");
 	}
 
   public void calcPoints(){
 
-    int r = 100; // radius
-    int[] pLight = {0,0,250}; // light position
-    int[] pCamera = {0,0,200}; // camera/eye position
+    double r = 100; // radius
+    double[] pLight = {0,0,250}; // light position
+    double[] pCamera = {0,0,200}; // camera/eye position
 
     // calculate y points
-    for(int y=-100; y<=100; y++){
+    for(double y=-100; y<=100; y++){
       // calc x1, x2 points (x bounds)
-      int x1 = (int)Math.sqrt(Math.pow(r,2)-Math.pow(y,2));
-      int x2 = -x1;
+      double x1 = (double)Math.sqrt(Math.pow(r,2)-Math.pow(y,2));
+      double x2 = -x1;
       // got x
-      for(int x=x2; x<=x1; x++){
+      for(double x=x2; x<=x1; x++){
         // calculate z
-        int z = (int)Math.sqrt(Math.pow(r,2)-Math.pow(y,2)-Math.pow(x,2));
+        double z = (double)Math.sqrt(Math.pow(r,2)-Math.pow(y,2)-Math.pow(x,2));
         // find and normalize vector
-        double l = Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2));
-        double[] n = {x,y,z};
-        for(double val: n) val = val/l;
-        //
+        double[] n = normlz(new double[]{x,y,z});
+        // do light calculations
+
+        // pointCamera - point   unit vector v
+        double[] v = normlz(new double[]{pCamera[0]-x,pCamera[1]-y,pCamera[2]-z});
+        // pointCamera - point   unit vector L
+        double[] l = normlz(new double[]{pLight[0]-x,pLight[1]-y,pLight[2]-z});
+
+        double ndotL = n[0]*l[0]+n[1]*l[1]+n[2]*l[2];
+        // reflected array r
+        double[] ray = new double[3];
+        for(int i=0;i<3;i++) ray[i] = 2*ndotL*n[i]-l[i];
+        double rayDotV = ray[0]*v[0]+ray[1]*v[1]+ray[2]*v[2];
+
+        // calculate intensities
+
       }
     }
 
+  }
+
+  public double[] normlz(double[] v){
+    double l = Math.sqrt(Math.pow(v[0],2)+Math.pow(v[1],2)+Math.pow(v[2],2));
+    for(int i=0;i<v.length;i++) v[i] = v[i]/l;
+    return v;
   }
 
 }// SimpleCanvas
