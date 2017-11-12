825b58826ab177b186c27b02bc482ec5721bd2f7
Marvelousmishig <dmishig@gmail.com>
2017-11-12 12:57:25 -0700

    feat: color change based on size

--- 11 - FinalProject/Canvas.java
+++ 11 - FinalProject/Canvas.java
@@ -17,28 +17,42 @@ import java.util.HashMap;
 class Particle{
 
   Ellipse2D.Double shape;
-  Color color;
+  Color color1,color2;
   double[] direction;
   double x=0,y=0;
   double size = 8.0;
   Random ran;
   double speed;
 
-  public Particle(Random _ran, double _speed){
+  public Particle(Random _ran, double _speed, Color _color1,Color _color2){
     ran = _ran;
     shape = new Ellipse2D.Double(x,y,size,size);
-    color = Color.white;
     // generate vector
     double speed = _speed;
     double xD = ranDouble(-speed,speed);
     double yD = ranDouble(-speed,speed);
     direction = new double[]{xD,yD};
-
+    color1 = _color1;
+    color2 = _color2;
   }
 
   public void draw(Graphics2D g2d){
-    g2d.setColor(color);
+    Color c = getColor();
+    g2d.setColor(c);
+    // get color
     g2d.fill(shape);
+  }
+  private Color getColor(){
+    // get colors
+    double r1 = color1.getRed(), g1 = color1.getGreen(), b1 = color1.getBlue();
+    double r2 = color2.getRed(), g2 = color2.getGreen(), b2 = color2.getBlue();
+    // interpolate between colors
+    double l = size/13.0;
+    double ll = 1.0 - l;
+    double r3 = l*r1 + ll*r2;
+    double g3 = l*g1 + ll*g2;
+    double b3 = l*b1 + ll*b2;
+    return new Color((int)r3,(int)g3,(int)b3);
   }
   public void update(double xforce, double yforce){
     double r1 = ranDouble(-.2,.2);
@@ -71,8 +85,10 @@ class Emitter{
   }
   public void emit(int n){
     double speed = (double)state.get("speed");
+    Color color1 = (Color)state.get("color1");
+    Color color2 = (Color)state.get("color2");
     for(int i=0; i<n; i++){
-      Particle particle = new Particle(ran,speed);
+      Particle particle = new Particle(ran,speed,color1,color2);
       particles.add(particle);
     }
   }
@@ -105,11 +121,7 @@ public class Canvas extends JPanel{
 		setBackground(Color.black);
 
     state = new HashMap();
-    state.put("speed",5.0);
-    state.put("xforce",0.0);
-    state.put("yforce",0.0);
-    state.put("color1",Color.white);
-    state.put("color2",Color.white);
+    initState();
 
     em = new Emitter(state);
 
@@ -117,6 +129,14 @@ public class Canvas extends JPanel{
         public void actionPerformed(ActionEvent evt){ repaint(); }
     });
     timer.start();
+  }
+
+  public void initState(){
+    state.put("speed",5.0);
+    state.put("xforce",0.0);
+    state.put("yforce",0.0);
+    state.put("color1",Color.white);
+    state.put("color2",Color.white);
   }
 
   public void paintComponent(Graphics g){
