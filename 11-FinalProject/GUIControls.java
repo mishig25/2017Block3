d0eb82905747de542c4fc20719ae6f7968113ac7
Marvelousmishig <dmishig@gmail.com>
2017-11-13 22:03:54 -0700

    feat: presets

--- 11-FinalProject/GUIControls.java
+++ 11-FinalProject/GUIControls.java
@@ -13,7 +13,7 @@ class GUIControls extends JPanel implements ChangeListener{
 
   // instance vars
 	JSlider sSlider,ySlider,xSlider,fSlider; // slider for moving light source
-	JLabel rlabel0,rlabel1,rlabel2,rlabel3,rlabel4,rlabel5; // label
+	JLabel rlabel0,rlabel1,rlabel2,rlabel3,rlabel4,rlabel5,rlabel6; // label
   JButton color1,color2;
 
 	// current instance of canvas so that
@@ -43,6 +43,7 @@ class GUIControls extends JPanel implements ChangeListener{
     rlabel3 = new JLabel("Force Y:");
     rlabel4 = new JLabel("Emit Frequency:");
     rlabel5 = new JLabel("Emitter Type:");
+    rlabel6 = new JLabel("Preset:");
 
     // init buttons
     color1 = new JButton("Choose Color1");
@@ -50,7 +51,7 @@ class GUIControls extends JPanel implements ChangeListener{
     initColorButton(color1,"color1");
     initColorButton(color2,"color2");
 
-    //
+    // emitter types
     String[] emitterTypesNames = {"Point","Plane"};
     JComboBox emitterTypes = new JComboBox(emitterTypesNames);
       // add ActionListener to JComboBox
@@ -61,14 +62,26 @@ class GUIControls extends JPanel implements ChangeListener{
           }
       });
 
+    // choose preset
+    String[] presetTypesName = {"None","Flame", "Snow"};
+    JComboBox presetTypes = new JComboBox(presetTypesName);
+      // add ActionListener to JComboBox
+      presetTypes.addActionListener (new ActionListener () {
+          public void actionPerformed(ActionEvent e) {
+            String chosenPreset = (String)presetTypes.getSelectedItem();
+            if(chosenPreset == "Flame") presetFlame();
+            if(chosenPreset == "Snow") presetSnow();
+          }
+      });
+
 		// create JPanel and add all the components to it
 		JPanel r = new JPanel();
 		r.setLayout(new BoxLayout(r, BoxLayout.Y_AXIS));
 
     // add components to the parent JPanel
     for(JComponent component: new JComponent[]{rlabel0,rlabel4,fSlider,rlabel5,emitterTypes,
-      rlabel1,sSlider,rlabel2,xSlider,
-      rlabel3,ySlider,color1,color2}) r.add(component);
+      rlabel1,sSlider,rlabel2,xSlider,rlabel3,
+      ySlider,color1,color2,rlabel6,presetTypes}) r.add(component);
 
 		add(r); // add the newly created JPanel to this class
 
@@ -110,6 +123,30 @@ class GUIControls extends JPanel implements ChangeListener{
     button.setBackground(Color.white);
     button.setOpaque(true);
     button.setBorderPainted(false);
+  }
+
+  public void presetFlame(){
+    myCanvas.state.put("speed",2.0);
+    myCanvas.state.put("xforce",0.0);
+    myCanvas.state.put("yforce",-0.3);
+    myCanvas.state.put("emfr",61);
+    myCanvas.state.put("x",-18);
+    myCanvas.state.put("y",284);
+    myCanvas.state.put("emtype","Point");
+    myCanvas.state.put("color1",new Color(255,102,51));
+    myCanvas.state.put("color2",new Color(255,255,102));
+  }
+
+  public void presetSnow(){
+    myCanvas.state.put("speed",6.0);
+    myCanvas.state.put("xforce",0.0);
+    myCanvas.state.put("yforce",0.6);
+    myCanvas.state.put("emfr",20);
+    myCanvas.state.put("x",0);
+    myCanvas.state.put("y",-340);
+    myCanvas.state.put("emtype","Plane");
+    myCanvas.state.put("color1",Color.white);
+    myCanvas.state.put("color2",Color.white);
   }
 
   private class ButtonListener implements ActionListener {
